481456bc2725cc4d7f0d5ebdf52367c7941893a3
ch4r1ty <920853279@qq.com>
2023-07-31 10:14:30 +0800

    07/31/2023

--- README.md
+++ README.md
@@ -2,19 +2,33 @@
 ## WHAT IS IT
 Face-Match is a program aiming at face comparison
 
+
+
 ## TOOLS
 [pandas](https://pandas.pydata.org/)
 
 [face_recognition](https://github.com/ageitgey/face_recognition)
 
 
+
+
 ## FILE DESCRIPTION
 **‘main.py’**: Do not read it plz, it's completely shitty code!🤮🤮 It is where the coder first coded all the lines in one python file
 
-**’app.py‘**: Linked to 'main.py'🤮🤮
+**‘app.py’**: Linked to 'main.py'🤮🤮
 
 **‘function.py’**: The coder packages functions in 'main.py' in one function😊😊
 
 **’test.py‘**: It is the most vital code in the project! Many little functions are decomposed from one single function in 'function.py'🥵🥵
 
 **‘app-test’**: Linked to 'test.py', not bad🥵🥵
+
+
+
+---
+
+## UPDATE
+
+### 07/31/2023
+
+To improve the efficiency of the program, by saving face-encodings in a new file: **'face_encodings.pkl'**, instead of getting all the know_face_encodings each time when post a request.
--- test.py
+++ test.py
@@ -5,12 +5,20 @@ import pickle
 
 
 def check_image_exists(image_path):
+    """
+    检查给定路径的图像文件是否存在。
+    如果文件不存在，抛出 FileNotFoundError。
+    """
     if not os.path.exists(image_path):
         raise FileNotFoundError("No such img.")
     return True
 
 
 def load_image_and_generate_encodings(image_path):
+    """
+    加载给定路径的图像文件，并生成面部编码。
+    如果图像中没有面部，抛出 ValueError。
+    """
     image = face_recognition.load_image_file(image_path)
     face_encodings = face_recognition.face_encodings(image)
     if not face_encodings:
@@ -19,6 +27,10 @@ def load_image_and_generate_encodings(image_path):
 
 
 def get_known_face_encodings(folder_path, file_path):
+    """
+    从给定的文件夹路径加载已知的面部编码，或者从给定的文件路径加载已保存的面部编码。
+    如果文件不存在，将从文件夹中的图像生成面部编码，并保存到文件中。
+    """
     if os.path.exists(file_path):
         with open(file_path, 'rb') as f:
             known_face_encodings = pickle.load(f)
@@ -36,6 +48,10 @@ def get_known_face_encodings(folder_path, file_path):
 
 
 def compare_faces(unknown_face_encodings, known_face_encodings):
+    """
+    比较未知的面部编码和已知的面部编码。
+    如果面部匹配，将匹配的面部添加到列表中。
+    """
     matched_faces = []
     for unknown_face_encoding in unknown_face_encodings:
         for filename, known_face_encoding in known_face_encodings.items():
@@ -46,6 +62,9 @@ def compare_faces(unknown_face_encodings, known_face_encodings):
 
 
 def get_person_info(matched_faces):
+    """
+    从Excel文件中获取匹配的面部的信息。
+    """
     df = pd.read_excel('info.xlsx')
     matched_persons = []
     for person_id in matched_faces:
@@ -56,6 +75,9 @@ def get_person_info(matched_faces):
 
 
 def find_person_info(image_path):
+    """
+    主函数，调用上述函数进行人脸识别。
+    """
     check_image_exists(image_path)
     unknown_face_encodings = load_image_and_generate_encodings(image_path)
     known_face_encodings = get_known_face_encodings("db", "face_encodings.pkl")
