0740b77f54489d8cc7b357cccb5b18a4e22f8885
ch4r1ty <920853279@qq.com>
2023-08-02 09:33:15 +0800

    08/02/2023

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, request, jsonify
-from main import find_person_info  # 确保你的function.py文件在同一目录下，或者在Python的搜索路径中
+from flask import Flask, request, jsonify, render_template
+# from werkzeug.utils import secure_filename
+from main import find_person_info  # 确保你的main.py文件在同一目录下，或者在Python的搜索路径中
+# from io import BytesIO
 
 app = Flask(__name__)
 
 
-@app.route('/find_person_info', methods=['POST'])
+@app.route('/find_person_info', methods=['POST', 'GET'])
 def find_person_info_api():
-    image_path = request.json['image_path']
+    if 'image' not in request.files:
+        return jsonify({"message": "No image file in the request."}), 400
+    file = request.files['image']
+
     try:
-        persons_info = find_person_info(image_path)
+        persons_info = find_person_info(file.stream)
         return jsonify(persons_info), 200
     except FileNotFoundError:
         return jsonify({"message": "The image does not exist."}), 200
@@ -17,8 +22,14 @@ def find_person_info_api():
             return jsonify({"message": "No faces found in the uploaded image."}), 200
         elif "No faces found in the image" in str(e):
             return jsonify({"message": "No faces found in one of the known images."}), 200
-    except Exception:
-        return jsonify({"message": "No match found."}), 200
+        return jsonify(persons_info), 200
+    except Exception as e:
+        return jsonify({"message": e}), 200
+
+
+@app.route('/', methods=['GET'])
+def upload_form():
+    return render_template('upload.html')
 
 
 if __name__ == '__main__':
